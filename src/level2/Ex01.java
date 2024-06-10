3afc02d20f3435072005e7bea95bef9f7f2457c4
JunBe <wnsqowid0901@gmail.com>
2024-06-10 15:56:18 +0900

    스킬트리

---
+++ src/level2/Ex01.java
@@ -0,0 +1,36 @@
+package level2;
+//스킬트리
+//https://school.programmers.co.kr/learn/courses/30/lessons/49993
+public class Ex01 {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        // skill : 선행스킬 순서 / 1 ~ 26
+        // skill_trees[] : 스킬트리 / 배열 크기 : 1 ~ 20 , 원소 길이 : 2 ~ 26 / 스킬 중복 x
+        //C - B - D : CBD
+
+        for (String skills : skill_trees) { //스킬트리 배열 반복
+            int i = 0; // 선행스킬 인덱스 조작
+            boolean tf = true; // 스킬트리 참거짓 여부
+            for (char x : skills.toCharArray()) { //스킬트리의 스킬배열 반복
+                if (skill.indexOf(x) != -1) { //x가 선행스킬이라면
+                    if (skill.charAt(i) == x) { //선행스킬의 순서를 만족해야함
+                        i++;
+                    } else { // 다르면 스킬트리가 아님
+                        tf = false;
+                        break;
+                    }
+                }
+            }
+
+            if (tf) { // 스킬트리라면
+                answer++; // answer 카운트
+            }
+        }
+
+        return answer; //가능한 스킬트리 개수
+    }
+    public static void main(String[] args) {
+        String s = "BACDE";
+        System.out.println(s.indexOf("Z"));
+    }
+}

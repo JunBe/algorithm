1358796b59223c92bf15ee1e3eb608b713205b92
JunBe <wnsqowid0901@gmail.com>
2024-06-26 21:15:25 +0900

    문자열 출력하기

--- string/ex01.js
+++ string/ex01.js
@@ -1,1 +1,6 @@
-// hello dz
+//문자열 출력하기
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+}).on('line', console.log)

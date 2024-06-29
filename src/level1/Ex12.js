cd03bca781e2923400651ec4d7a9bd4f79c59b51
JunBe <wnsqowid0901@gmail.com>
2024-06-29 11:37:52 +0900

    두 개 뽑아서 더하기

---
+++ src/level1/Ex12.js
@@ -0,0 +1,18 @@
+//두 개 뽑아서 더하기
+function solution(numbers) {
+    var answer = [];
+    let arr=[];
+    for(let i=0; i<numbers.length; i++){
+        for(let j=i+1; j<numbers.length; j++){
+            arr.push(numbers[i]+numbers[j]);
+        }
+    }
+    arr.sort((a,b)=>a-b);
+    
+    var set = new Set(arr);
+    for(const item of set){
+        answer.push(item);
+    }
+    
+    return answer;
+}

4f755a72d1b35dd5f8fec38c902253e7ffbd9018
JunBe <wnsqowid0901@gmail.com>
2024-11-20 23:55:38 +0900

    알고리즘의 수행 시간 3

---
+++ baekjoon/src/step11/Ex03.java
@@ -0,0 +1,13 @@
+package step11;
+
+import java.util.Scanner;
+
+//24264 알고리즘 수업 - 알고리즘의 수행 시간 3
+public class Ex03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        System.out.println(n * n);
+        System.out.println(2);
+    }
+}

119198dd0deca22f89d00f78777ca6de54847db6
JunBe <wnsqowid0901@gmail.com>
2024-11-30 09:15:36 +0900

    알고리즘의 수행 시간 5

---
+++ baekjoon/src/step11/Ex05.java
@@ -0,0 +1,13 @@
+package step11;
+
+import java.util.Scanner;
+
+//24265 알고리즘 수업 - 알고리즘의 수행 시간 5
+public class Ex05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        System.out.println(n * n * n);
+        System.out.println(3);
+    }
+}

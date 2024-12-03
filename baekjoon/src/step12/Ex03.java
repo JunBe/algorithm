be7b845b4375ba1733bceacadf99e71273957074
JunBe <wnsqowid0901@gmail.com>
2024-12-03 20:34:42 +0900

    수학은 비대면 강의입니다.

---
+++ baekjoon/src/step12/Ex03.java
@@ -0,0 +1,24 @@
+package step12;
+
+import java.util.Scanner;
+
+//19532 수학은 비대면강의입니다.
+public class Ex03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+        for (int x = -999; x <= 999; x++) {
+            for (int y = -999; y <= 999; y++) {
+                if (a * x + b * y == c && d * x + e * y == f) {
+                    System.out.println(x + " " + y);
+                    return;
+                }
+            }
+        }
+    }
+}

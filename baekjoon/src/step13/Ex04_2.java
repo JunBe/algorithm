e5c4f0b7b4c0aac2ec9371502e5f73344088661d
JunBe <wnsqowid0901@gmail.com>
2024-12-09 12:55:40 +0900

    수 정렬하기 다른 풀이

---
+++ baekjoon/src/step13/Ex04_2.java
@@ -0,0 +1,27 @@
+package step13;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+
+//2751 수 정렬하기
+public class Ex04_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for (int val : arr) {
+            bw.write(val + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
